798f3b05cad59806501d7a15f107f420a57e6a72
Daniel otowski <danielotowski@googlemail.com>
2024-04-07 19:20:33 +0000

    Add logo and font awesome icons to navbar, add favicon

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,21 +1,31 @@
-import React from 'react'
-import { Navbar, Container, Nav} from 'react-bootstrap';
+import React from "react";
+import { Navbar, Container, Nav } from "react-bootstrap";
+import logo from "../assets/pooch-book-logo.png";
 
 const NavBar = () => {
   return (
-    <Navbar expand="md" fixed='top'>
+    <Navbar expand="md" fixed="top">
       <Container>
-        <Navbar.Brand href="#home">React-Bootstrap</Navbar.Brand>
+        <Navbar.Brand>
+          <img src={logo} alt="logo" height="90" />
+        </Navbar.Brand>
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="mr-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
+            <Nav.Link>
+              <i className="fa-solid fa-house-chimney"></i>Home
+            </Nav.Link>
+            <Nav.Link>
+              <i className="fa-solid fa-right-to-bracket"></i>Sign In
+            </Nav.Link>
+            <Nav.Link>
+              <i className="fa-solid fa-user-plus"></i>Sign Up
+            </Nav.Link>
           </Nav>
         </Navbar.Collapse>
       </Container>
     </Navbar>
   );
-}
+};
 
-export default NavBar
+export default NavBar;
