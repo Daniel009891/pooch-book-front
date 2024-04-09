7e912f56305de352d6eeb93e3ae242054c7a0f6e
Daniel otowski <danielotowski@googlemail.com>
2024-04-09 19:41:12 +0000

    Install axios and set variables in axiosDefaults.js

--- src/App.js
+++ src/App.js
@@ -1,7 +1,8 @@
 import { Container } from 'react-bootstrap';
 import styles from './App.module.css';
 import NavBar from './components/NavBar';
-import {Route,Switch} from 'react-router-dom'
+import {Route,Switch} from 'react-router-dom';
+import './api/axiosDefaults';
 
 
 function App() {
