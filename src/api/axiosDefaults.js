a7b9c493b005559f58a6aa035a95bf9345b1b490
Daniel otowski <danielotowski@googlemail.com>
2024-04-14 10:00:53 +0000

    Add axios interceptor for refresh token

--- src/api/axiosDefaults.js
+++ src/api/axiosDefaults.js
@@ -4,3 +4,5 @@ axios.defaults.baseURL = "https://pooch-book-api-b940e6d3707a.herokuapp.com/";
 axios.defaults.headers.post['Content-Type'] = 'multipart/form-data'
 axios.defaults.withCredentials = true
 
+export const axiosReq = axios.create();
+export const axiosRes = axios.create();
