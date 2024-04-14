4ae15cc1aed222c6e0c492544db336a919307b0d
Daniel otowski <danielotowski@googlemail.com>
2024-04-14 11:13:54 +0000

    Add avatar function to set props and include it within navbar.js

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -5,6 +5,7 @@ import styles from "../styles/NavBar.module.css";
 import { NavLink } from "react-router-dom";
 
 import { useCurrentUser } from "../contexts/CurrentUserContext";
+import Avatar from "./Avatar";
 
 const NavBar = () => {
   const addPostIcon = (
@@ -40,7 +41,7 @@ const NavBar = () => {
         className={styles.NavLink}
         to={`/profiles/${currentUser?.profile_id}`}
       >
-        <img src={currentUser?.profile_image} />
+        <Avatar src={currentUser?.profile_image} text='Profile' height={40}/>
       </NavLink>
     </>
   );
