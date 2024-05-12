44475113eb94a1701e473f6cb3e5b74e9d1bfd1e
Daniel otowski <danielotowski@googlemail.com>
2024-05-12 11:13:09 +0000

    Add card body and display details of users post if they are owner

--- src/pages/posts/Post.js
+++ src/pages/posts/Post.js
@@ -14,24 +14,38 @@ const Post = (props) => {
     comments_count,
     like_id,
     title,
+    content,
     image,
     updated_at,
+    postPage,
   } = props;
 
   const currentUser = useCurrentUser();
   const is_owner = currentUser?.username === owner
 
-  return <Card className={styles.Post}>
-    <Card.Body>
-      <Media className="align-items-center justify-content-between">
-        <Link to={`/profiles/${profile_id}`}>
-          <Avatar src={profile_image} height={55} />
-          {owner}
-        </Link>
-        
-      </Media>
-    </Card.Body>
-  </Card>
+  return (
+    <Card className={styles.Post}>
+      <Card.Body>
+        <Media className="align-items-center justify-content-between">
+          <Link to={`/profiles/${profile_id}`}>
+            <Avatar src={profile_image} height={55} />
+            {owner}
+          </Link>
+          <div className="d-flex align-items-center">
+            <span>{updated_at}</span>
+            {is_owner && postPage && "..."}
+          </div>
+        </Media>
+      </Card.Body>
+      <Link to={`posts/${id}`}>
+        <Card.Img src={image} alt={title} />
+      </Link>
+      <Card.Body>
+        {title && <Card.Title className="text-center">{title}</Card.Title>}
+        {content && <Card.Text>{content}</Card.Text>}
+      </Card.Body>
+    </Card>
+  );
 };
 
 export default Post;
