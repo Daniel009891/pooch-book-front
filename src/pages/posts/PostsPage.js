71cb3fbe35155952d29f9bfc2c81b7a2e7df7d20
Daniel otowski <danielotowski@googlemail.com>
2024-05-12 18:54:23 +0000

    Add props to posts page ternary statement for infinite scroll

--- src/pages/posts/PostsPage.js
+++ src/pages/posts/PostsPage.js
@@ -66,6 +66,10 @@ function PostsPage({ message, filter = "" }) {
                 children={posts.results.map((post) => (
                   <Post key={post.id} {...post} setPosts={setPosts} />
                 ))}
+                dataLength={posts.results.lengt}
+                loader={<Asset spinner />}
+                hasMore={!!posts.next}
+                next={() => {}}
               />
             ) : (
               <Container className={appStyles.Content}>
