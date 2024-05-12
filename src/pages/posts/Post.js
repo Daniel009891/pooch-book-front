070e6fff892379d086eb238bb44a9416db29d383
Daniel otowski <danielotowski@googlemail.com>
2024-05-12 11:01:24 +0000

    Add card and display profile id, profile image to post.js

--- src/pages/posts/Post.js
+++ src/pages/posts/Post.js
@@ -1,6 +1,9 @@
 import React from "react";
 import styles from "../../styles/Post.module.css";
 import { useCurrentUser } from "../../contexts/CurrentUserContext";
+import { Card, Media } from "react-bootstrap";
+import { Link } from "react-router-dom/cjs/react-router-dom.min";
+import Avatar from "../../components/Avatar";
 
 const Post = (props) => {
   const {
@@ -18,7 +21,17 @@ const Post = (props) => {
   const currentUser = useCurrentUser();
   const is_owner = currentUser?.username === owner
 
-  return <div>Post displays here</div>;
+  return <Card className={styles.Post}>
+    <Card.Body>
+      <Media className="align-items-center justify-content-between">
+        <Link to={`/profiles/${profile_id}`}>
+          <Avatar src={profile_image} height={55} />
+          {owner}
+        </Link>
+        
+      </Media>
+    </Card.Body>
+  </Card>
 };
 
 export default Post;
