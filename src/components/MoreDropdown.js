6264730302262d40b47635b150d0e47f4bc45247
Daniel otowski <danielotowski@googlemail.com>
2024-05-13 19:52:57 +0000

    Add handle delete function to dropdown icon

--- src/components/MoreDropdown.js
+++ src/components/MoreDropdown.js
@@ -16,7 +16,7 @@ const ThreeDots = React.forwardRef(({ onClick }, ref) => (
   />
 ));
 
-export const MoreDropdown = ({handleEdit}) => {
+export const MoreDropdown = ({handleEdit, handleDelete}) => {
   return (
     <Dropdown className="ml-auto" drop="left">
       <Dropdown.Toggle as={ThreeDots} />
@@ -34,7 +34,7 @@ export const MoreDropdown = ({handleEdit}) => {
         </Dropdown.Item>
         <Dropdown.Item
           className={styles.DropddownItem}
-          onClick={() => {}}
+          onClick={handleDelete}
           aria-label="delete"
         >
           <i className="fas fa-trash-alt" />
