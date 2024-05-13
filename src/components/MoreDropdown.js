db20c82ca9fe6e791537b4454a1a36588fbba255
Daniel otowski <danielotowski@googlemail.com>
2024-05-13 19:30:31 +0000

    Add fontawesome icon, add moredropdown to post.js

--- src/components/MoreDropdown.js
+++ src/components/MoreDropdown.js
@@ -1,8 +1,10 @@
-import styles from "../styles/MoreDropdown.module.css";
 import React from "react";
 import { Dropdown } from "react-bootstrap";
+import styles from "../styles/MoreDropdown.module.css";
+
 // The forwardRef is important!!
 // Dropdown needs access to the DOM node in order to position the Menu
+
 const ThreeDots = React.forwardRef(({ onClick }, ref) => (
   <i
     className="fas fa-ellipsis-v"
@@ -15,21 +17,23 @@ const ThreeDots = React.forwardRef(({ onClick }, ref) => (
 ));
 
 export const MoreDropdown = () => {
-    return (
-      <Dropdown>
-        <Dropdown.Toggle as={CustomToggle} id="dropdown-custom-components">
-          Custom toggle
-        </Dropdown.Toggle>
+  return (
+    <Dropdown>
+      <Dropdown.Toggle as={ThreeDots} id="dropdown-custom-components">
+        Custom toggle
+      </Dropdown.Toggle>
 
-        <Dropdown.Menu as={CustomMenu}>
-          <Dropdown.Item eventKey="1">Red</Dropdown.Item>
-          <Dropdown.Item eventKey="2">Blue</Dropdown.Item>
-          <Dropdown.Item eventKey="3" active>
-            Orange
-          </Dropdown.Item>
-          <Dropdown.Item eventKey="1">Red-Orange</Dropdown.Item>
-        </Dropdown.Menu>
-      </Dropdown>
-    );
-}
-
+      <Dropdown.Menu
+        className="text-center"
+        popperConfig={{ strategy: "fixed" }}
+      >
+        <Dropdown.Item eventKey="1">Red</Dropdown.Item>
+        <Dropdown.Item eventKey="2">Blue</Dropdown.Item>
+        <Dropdown.Item eventKey="3" active>
+          Orange
+        </Dropdown.Item>
+        <Dropdown.Item eventKey="1">Red-Orange</Dropdown.Item>
+      </Dropdown.Menu>
+    </Dropdown>
+  );
+};
