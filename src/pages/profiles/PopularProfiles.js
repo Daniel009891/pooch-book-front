b5bc27d2ec9673ee2fb06ea5dd313d81b79c8440
Daniel otowski <danielotowski@googlemail.com>
2024-05-16 20:18:35 +0000

    Add popular profiles function and use effect hook

--- src/pages/profiles/PopularProfiles.js
+++ src/pages/profiles/PopularProfiles.js
@@ -1,11 +1,42 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import appStyles from "../../App.module.css";
 import { Container } from "react-bootstrap";
+import { axiosReq } from "../../api/axiosDefaults";
+import { useCurrentUser } from "../../contexts/CurrentUserContext";
 
 const PopularProfiles = () => {
+  const [profileData, setProfileData] = useState({
+    pageProfile: { results: [] },
+    PopularProfiles: { results: [] },
+  });
+
+  const { PopularProfiles } = profileData;
+  const currentUser = useCurrentUser()
+
+  useEffect(() => {
+    const handleMount = async () => {
+      try {
+        const { data } = await axiosReq.get(
+          "/profiles/?ordering=-followers_count"
+        );
+        setProfileData(prevState => ({
+            ...prevState,
+            PopularProfiles: data,
+        }))
+      } catch (err) {
+        console.log(err)
+      }
+    };
+
+    handleMount()
+  }, [currentUser]);
+
   return (
     <Container className={appStyles.Content}>
       <p>Most followed profiles.</p>
+      {PopularProfiles.results.map(profile => (
+        <p key={profile.id}>{profile.owner}</p>
+      ))}
     </Container>
   );
 };
