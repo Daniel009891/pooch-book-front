b3661bcc70913513be3fdec6e89006e40976ac5d
Daniel otowski <danielotowski@googlemail.com>
2024-05-16 20:07:36 +0000

    Add profiles folder and popularprofiles.js

--- src/pages/posts/PostPage.js
+++ src/pages/posts/PostPage.js
@@ -79,7 +79,7 @@ function PostPage() {
         </Container>
       </Col>
       <Col lg={4} className="d-none d-lg-block p-0 p-lg-2">
-        Popular profiles for desktop
+        <p>Popular profiles for desktop</p>
       </Col>
     </Row>
   );
