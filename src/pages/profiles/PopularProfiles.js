3c741a79439d7e4f7897f3f8fcdcab0a9e23ffd2
Daniel otowski <danielotowski@googlemail.com>
2024-05-18 17:47:08 +0000

    Add Profile data context and add logic to display across the app

--- src/pages/profiles/PopularProfiles.js
+++ src/pages/profiles/PopularProfiles.js
@@ -1,37 +1,12 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 import appStyles from "../../App.module.css";
 import { Container } from "react-bootstrap";
-import { axiosReq } from "../../api/axiosDefaults";
-import { useCurrentUser } from "../../contexts/CurrentUserContext";
 import Asset from "../../components/Asset";
 import Profile from "./Profile";
+import { useProfileData } from "../../contexts/ProfileDataContext";
 
 const PopularProfiles = ({ mobile }) => {
-  const [profileData, setProfileData] = useState({
-    pageProfile: { results: [] },
-    PopularProfiles: { results: [] },
-  });
-
-  const { PopularProfiles } = profileData;
-  const currentUser = useCurrentUser();
-
-  useEffect(() => {
-    const handleMount = async () => {
-      try {
-        const { data } = await axiosReq.get(
-          "/profiles/?ordering=-followers_count"
-        );
-        setProfileData((prevState) => ({
-          ...prevState,
-          PopularProfiles: data,
-        }));
-      } catch (err) {
-        console.log(err);
-      }
-    };
-
-    handleMount();
-  }, [currentUser]);
+  const { popularProfiles } = useProfileData();
 
   return (
     <Container
@@ -39,18 +14,18 @@ const PopularProfiles = ({ mobile }) => {
         mobile && "d-lg-none text-center mb-3"
       }`}
     >
-      {PopularProfiles.results.length ? (
+      {popularProfiles.results.length ? (
         <>
           <p>Most followed profiles.</p>
           {mobile ? (
             <div className="d-flex justify-content-around">
-              {PopularProfiles.results.slice(0, 4).map((profile) => (
+              {popularProfiles.results.slice(0, 4).map((profile) => (
                 <Profile key={profile.id} profile={profile} mobile />
               ))}
             </div>
           ) : (
-            PopularProfiles.results.map((profile) => (
-              <Profile key={profile.id} profile={profile}  />
+            popularProfiles.results.map((profile) => (
+              <Profile key={profile.id} profile={profile} />
             ))
           )}
         </>
